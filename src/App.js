21a54db090489aca270aa0a133b33b2cc642449e
Jatin.Kapoor <jatin.kapoor@target.com>
2018-06-24 22:23:23 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Paper>
+          <p> My Game App </p>
+        </Paper>  
+      </div>
+    )
+  }
+
+}
+
+export default App;
